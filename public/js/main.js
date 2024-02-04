f94924011e9324df8b00d823a07f579ef2dcbf77
KoY877 <yaoferdinand@outlook.de>
2024-02-04 20:34:32 +0100

    Traitement de la mentions allergie en JS

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+
 /* ============================================================
    * Gérer les nombre de convives dans le formulaire Reservation
    * ============================================================*/
@@ -31,15 +32,63 @@ const handleChange = (event) =>
             error.style.marginTop = -12 + "px";
             nombre.classList.add("is-invalid");
 
-        } else {
-            
+        } else {            
             error.innerHTML = `` ;
             error.style.display="none"
-            nombre.classList.remove("is-invalid");
-            
+            nombre.classList.remove("is-invalid");            
         }
     });
   
 }
 
 nombre.addEventListener("change", handleChange);
+
+
+/* ============================================================
+   * Gérer la mention de allergie dans le formulaire Reservation
+   * ============================================================*/
+
+    // Récupérer le champ mention des allergies   
+    let allergie = document.getElementById('mention_allergie');
+
+    //  Récupérer les données du champ choisie 
+    const mentionAllegieNon = document.getElementById('reservation_allergie_0');
+    const mentionAllegieOui = document.getElementById('reservation_allergie_1');
+    
+    // Récupérer la valeur du champ mention des allergies
+    const mentionAllegie = document.getElementById('reservation_mentionDesAllergies');
+
+   
+    const AllergieCheckedNon = (event) => {
+
+        // Annule l´envoi des données
+        event.preventDefault(); 
+
+        if (event.target.checked) {
+            // On cache le champs de saisie et on enlève la contrainte au formulaire
+            allergie.style.display =  'none';
+            allergie.required = false;
+            mentionAllegie.value = 'Pas d´allergie' ;
+        } 
+
+    }
+
+    mentionAllegieNon.addEventListener('change', AllergieCheckedNon);
+
+    const AllergieCheckedOui = (event) => {
+
+        // Annule l´envoi des données
+        event.preventDefault(); 
+    
+        if (event.target.checked) {
+            // Si c'est coché, on affiche le champs de saisie 
+            allergie.style.display =  "block";
+            allergie.required = true;
+            mentionAllegie.value = '';
+        }
+    }
+
+    mentionAllegieOui.addEventListener('change', AllergieCheckedOui);
+
+    
+
